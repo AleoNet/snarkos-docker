7b9b1c2d074c7aa6651448e31f975b459f56dbb6
jhaineault-aleo <jeff@aleo.org>
2025-06-17 16:25:09 -1000

    Updated workflow to build with arm and amd runners

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -11,30 +11,8 @@ fi
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$SCRIPT_DIR/envs"
 
-echo -n "Enter environment (canary, mainnet, testnet, test): "
-read ENV
+export GOOGLE_APPLICATION_CREDENTIALS="$PROJECT_ROOT/test/terraform-sa-key.json"
+echo "✅ Environment set to TEST"
 
-case "$ENV" in
-  canary)
-    export GOOGLE_APPLICATION_CREDENTIALS="$PROJECT_ROOT/canary/terraform-sa-key.json"
-    echo "✅ Environment set to CANARY"
-    ;;
-  mainnet)
-    export GOOGLE_APPLICATION_CREDENTIALS="$PROJECT_ROOT/mainnet/terraform-sa-key.json"
-    echo "✅ Environment set to MAINNET"
-    ;;
-  testnet)
-    export GOOGLE_APPLICATION_CREDENTIALS="$PROJECT_ROOT/testnet/terraform-sa-key.json"
-    echo "✅ Environment set to TESTNET"
-    ;;
-  test)
-    export GOOGLE_APPLICATION_CREDENTIALS="$PROJECT_ROOT/test/terraform-sa-key.json"
-    echo "✅ Environment set to TEST"
-    ;;
-  *)
-    echo "❌ Unknown environment: $ENV"
-    return 1
-    ;;
-esac
 
 echo "GOOGLE_APPLICATION_CREDENTIALS is now: $GOOGLE_APPLICATION_CREDENTIALS"
